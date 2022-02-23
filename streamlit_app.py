baf3ef93d09748b083c6b27e6f651790db2fa74e
Chibudu Nyiro <74914291+ch1b4d4@users.noreply.github.com>
2022-02-23 12:06:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,7 @@ st.write(" This is a comment line")
 
 historical_emissions = ('https://github.com/ch1b4d4/Stream-Lit-Practise/blob/main/historical_emissions.csv')
 
-def load_data(nrows):
-    data = pd.read_csv(historical_emissions, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    return data
+st.write(historical_emissions)
 
 
 
