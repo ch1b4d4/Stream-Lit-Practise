af4fd6ab064884f8bd695eff76c0846058d87cba
Chibudu Nyiro <74914291+ch1b4d4@users.noreply.github.com>
2022-02-23 11:49:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,11 @@ import numpy as np
 st.title('#PROFILING GREENHOUSE GAS EMISSIONS IN RELATION TO POLLUTION LEVELS IN EUROPEAN COUNTRIES')
 st.write(" This is a comment line")
 
+
+historical_emissions = ('/Users/la/Downloads/Clean Data/historical_emissions.csv')
+
+def load_data(nrows):
+    data = pd.read_csv(historical_emissions, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
