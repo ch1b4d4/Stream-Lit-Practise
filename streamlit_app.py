3e05b37e6fd502ca91bf45a6c190c2da1a8bf695
Chibudu Nyiro <74914291+ch1b4d4@users.noreply.github.com>
2022-02-23 11:26:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('#PROFILING GREENHOUSE GAS EMISSIONS IN RELATION TO POLLUTION LEVELS IN EUROPEAN COUNTRIES')
+st.write(" This is a comment line")
+
