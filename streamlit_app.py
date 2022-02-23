16417d4bb4a02fac00b73b0553cf46c5684bde05
Chibudu Nyiro <74914291+ch1b4d4@users.noreply.github.com>
2022-02-23 12:01:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,6 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     return data
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
 
 st.subheader('Raw data')
 st.write(data)
