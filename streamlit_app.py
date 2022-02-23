1990b739cacf070307c98448b82ae6d98df0c86a
Chibudu Nyiro <74914291+ch1b4d4@users.noreply.github.com>
2022-02-23 12:12:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ st.title('PROFILING GREENHOUSE GAS EMISSIONS IN RELATION TO POLLUTION LEVELS IN 
 st.write(" This is a comment line")
 
 
-historical_emissions = pd.read_csv("https://github.com/ch1b4d4/Stream-Lit-Practise/blob/main/historical_emissions.csv")
+historical_emissions = ("https://github.com/ch1b4d4/Stream-Lit-Practise/blob/main/historical_emissions.csv")
+pd.read_csv(historical_emissions)
 
 st.dataframe(historical_emissions)
 
